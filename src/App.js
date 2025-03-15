d6876949b27899c7419a467b148ffa6d40a8d50d
Leonardo Soplapuco <leonardosoplapuco@gmail.com>
2025-03-15 09:13:07 -0500

    0.0.3

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import PaginaPrincipal from "./Paginas/PaginaPrincipal/PaginaPrincipal";
+import PaginaDeCategoria from "./Paginas/Categorias/PaginaDeCategoria";
 
 import './App.css';
 
@@ -9,6 +10,9 @@ function App() {
         <Router>
             <Routes>
                 <Route path="/" element={<PaginaPrincipal/>}/>
+
+                {/* <Route path="/pagina-de-categoria/" element={<PaginaDeCategoria/>} /> */}
+                <Route path="/categoria/:categoria" element={<PaginaDeCategoria/>}/>
             </Routes>
         </Router>
     );
