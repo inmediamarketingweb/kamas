49df3e6b2cb9cc5d56b8cc647d0e986804e00f13
Leonardo Soplapuco <leonardosoplapuco@gmail.com>
2025-03-15 15:56:15 -0500

    0.0.5

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
+import { HelmetProvider } from 'react-helmet-async';
+
 import PaginaPrincipal from "./Paginas/PaginaPrincipal/PaginaPrincipal";
 import PaginaDeCategoria from "./Paginas/Categorias/PaginaDeCategoria";
 
@@ -7,14 +9,15 @@ import './App.css';
 
 function App() {
     return(
-        <Router>
-            <Routes>
+        <HelmetProvider>
+            <Router>
+                <Routes>
                 <Route path="/" element={<PaginaPrincipal/>}/>
 
-                {/* <Route path="/pagina-de-categoria/" element={<PaginaDeCategoria/>} /> */}
-                <Route path="/categoria/:categoria" element={<PaginaDeCategoria/>}/>
-            </Routes>
-        </Router>
+                <Route path="/productos/:categoria" element={<PaginaDeCategoria/>}/>
+                </Routes>
+            </Router>
+        </HelmetProvider>
     );
 }
 
