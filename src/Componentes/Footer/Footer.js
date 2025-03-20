787cf457d4250eeb39978abef015e4b2726bbc72
Leonardo Soplapuco <leonardosoplapuco@gmail.com>
2025-03-20 12:49:32 -0500

    0.2.4

--- src/Componentes/Footer/Footer.js
+++ src/Componentes/Footer/Footer.js
@@ -8,7 +8,108 @@ function Footer(){
             </a>
 
             <footer>
-                <div className='footer'>Footer</div>
+                <div className='footer block-content'>
+                    <div className='block-title-container'>
+                        <p className='block-title'>En KAMAS diseñamos tus sueños</p>
+                    </div>
+
+                    <nav className='footer-targets'>
+                        <div className='footer-target footer-target-1'>
+                            <p className='title'>Acerca de KAMAS</p>
+                            <ul>
+                                <li>
+                                    <a href='/nosotros/'>
+                                        <p>Nosotros</p>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href='/nosotros/'>
+                                        <p>Razones para comprar</p>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href='/nosotros/'>
+                                        <p>Propiedad intelectual</p>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href='/nosotros/'>
+                                        <p>Envios a Lima y Callao</p>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href='/nosotros/'>
+                                        <p>Envios a provincia</p>
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                        <div className='footer-target footer-target-2'>
+                            <p className='title'>Servicio al cliente</p>
+                            <ul>
+                                <li>
+                                    <a href='/nosotros/'>
+                                        <p>Nosotros</p>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href='/nosotros/'>
+                                        <p>Razones para comprar</p>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href='/nosotros/'>
+                                        <p>Propiedad intelectual</p>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href='/nosotros/'>
+                                        <p>Envios a Lima y Callao</p>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href='/nosotros/'>
+                                        <p>Envios a provincia</p>
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                        <div className='footer-target footer-target-3'>
+                            <p className='title'>Acerca de KAMAS</p>
+                            <ul>
+                                <li>
+                                    <a href='/nosotros/'>
+                                        <p>Nosotros</p>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href='/nosotros/'>
+                                        <p>Razones para comprar</p>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href='/nosotros/'>
+                                        <p>Propiedad intelectual</p>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href='/nosotros/'>
+                                        <p>Envios a Lima y Callao</p>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href='/nosotros/'>
+                                        <p>Envios a provincia</p>
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                        <div className='footer-target footer-target-1'>
+                            <p className='title'>Suscribete</p>
+                            <input type='mail'></input>
+                        </div>
+                    </nav>
+                </div>
             </footer>
         </>
     );
--- src/Paginas/PaginaPrincipal/PaginaPrincipal.js
+++ src/Paginas/PaginaPrincipal/PaginaPrincipal.js
@@ -1,3 +1,4 @@
+import { useState, useRef, useEffect } from 'react';
 import { Helmet } from 'react-helmet';
 
 import Header from '../../Componentes/Header/Header';
@@ -5,8 +6,36 @@ import Footer from '../../Componentes/Footer/Footer';
 
 import './PaginaPrincipal.css';
 
-function PaginaPrincipal(){
-    return(
+function PaginaPrincipal() {
+    const [categorias, setCategorias] = useState([]);
+    const [currentIndex, setCurrentIndex] = useState(0);
+    const sliderRef = useRef(null);
+    const totalSlides = 4;
+
+    useEffect(() => {
+        fetch('/assets/json/categorias/categorias.json')
+            .then(response => response.json())
+            .then(data => {
+                setCategorias(data.categorias.slice(0, 6));
+            })
+            .catch(error => console.error('Error cargando categorías:', error));
+    }, []);
+
+    useEffect(() => {
+        if (sliderRef.current) {
+            sliderRef.current.style.marginLeft = `-${currentIndex * 100}%`;
+        }
+    }, [currentIndex]);
+
+    const goToNextSlide = () => {
+        setCurrentIndex((prevIndex) => (prevIndex + 1) % totalSlides);
+    };
+
+    const goToPrevSlide = () => {
+        setCurrentIndex((prevIndex) => (prevIndex - 1 + totalSlides) % totalSlides);
+    };
+
+    return (
         <>
             <Helmet>
                 <title>Kamas | Fabricantes de colchones, camas y dormitorios.</title>
@@ -14,103 +43,49 @@ function PaginaPrincipal(){
 
             <Header/>
 
-            <main>
-                <div className='hero-container'>
-                    <section className='hero'>
-                        <div className='slider-container'>
-                            <ul className='slider'>
-                                <li>
-                                    <a href='/' title="1">
-                                        <img src="/assets/imagenes/componentes/slider/1.jpg" alt=""/>
+            <div className='hero-container'>
+                <section className='hero'>
+                    <div className='slider-container'>
+                        <ul className='slider' ref={sliderRef}>
+                            {[1, 2, 3, 4].map((num) => (
+                                <li key={num}>
+                                    <a href='/' title={num.toString()}>
+                                        <img src={`/assets/imagenes/componentes/slider/${num}.jpg`} alt={`Slide ${num}`} />
                                     </a>
                                 </li>
-                                <li>
-                                    <a href='/' title="2">
-                                        <img src="/assets/imagenes/componentes/slider/2.jpg" alt=""/>
-                                    </a>
-                                </li>
-                                <li>
-                                    <a href='/' title="3">
-                                        <img src="/assets/imagenes/componentes/slider/3.jpg" alt=""/>
-                                    </a>
-                                </li>
-                                <li>
-                                    <a href='/' title="4">
-                                        <img src="/assets/imagenes/componentes/slider/4.jpg" alt=""/>
-                                    </a>
-                                </li>
-                            </ul>
-                        </div>
-                    </section>
-
-                    <button className='hero-slider-button hero-slider-button-1'>
-                        <span className="material-icons">chevron_left</span>
-                    </button>
-                    <button className='hero-slider-button hero-slider-button-2'>
-                        <span className="material-icons">chevron_right</span>
-                    </button>
-                </div>
-
-                <div className='block-container'>
-                    <section className='block-content'>
-                        <div className='block-title-container'>
-                            <p className='block-title'>Nuestros productos</p>
-                        </div>
-                        <ul className='homepage-categories'>
-                            <li>
-                                <a href='/productos/colchones/'>
-                                    <div>
-                                        <img src="https://www.kamas.pe/829-large_default/colchon-kamas-extra-ortopedico-konig-queen.jpg" alt='Colchones | Kamas'/>
-                                    </div>
-                                    <p>Colchones</p>
-                                </a>
-                            </li>
-                            <li>
-                                <a href='/productos/cama-box-tarimas/'>
-                                    <div>
-                                        <img src="https://www.kamas.pe/2662-large_default/cama-box-king-americano-2-cuerpos-cabecera-pedestal-capitoneado-gris.jpg" alt='Cama box tarimas | Kamas'/>
-                                    </div>
-                                    <p>Cama box tarimas</p>
-                                </a>
-                            </li>
-                            <li>
-                                <a href='/productos/dormitorios/'>
-                                    <div>
-                                        <img src="https://www.kamas.pe/880-large_default/dormitorio-americano-king-colchon-kamas-sarki-cabecera-aerea-cuadros-1-puff-marron.jpg" alt='Dormitorios | Kamas'/>
-                                    </div>
-                                    <p>Dormitorios</p>
-                                </a>
-                            </li>
-                            <li>
-                                <a href='/productos/camas-funcionales/'>
-                                    <div>
-                                        <img src="https://www.kamas.pe/376-large_default/cama-box-espacio-americano-15-plazas-marron.jpg" alt='Camas funcionales | Kamas'/>
-                                    </div>
-                                    <p>Camas funcionales</p>
-                                </a>
-                            </li>
-                            <li>
-                                <a href='/productos/cabeceras/'>
-                                    <div>
-                                        <img src="https://www.kamas.pe/103-large_default/cabecera-pedestal-kamas-majestad-queen-violeta-oscuro.jpg" alt='Cabeceras | Kamas'/>
-                                    </div>
-                                    <p>Cabeceras</p>
-                                </a>
-                            </li>
-                            <li>
-                                <a href='/productos/sofas/'>
-                                    <div>
-                                        <img src="https://www.kamas.pe/566-large_default/sofa-seccional-kamas-beige.jpg" alt='Sofás | Kamas'/>
-                                    </div>
-                                    <p>Sofás</p>
-                                </a>
-                            </li>
+                            ))}
                         </ul>
-                    </section>
-                </div>
-            </main>
+                    </div>
+                </section>
+                <button className='hero-slider-button hero-slider-button-1' onClick={goToPrevSlide}>
+                    <span className='material-icons'>chevron_left</span>
+                </button>
+                <button className='hero-slider-button hero-slider-button-2' onClick={goToNextSlide}>
+                    <span className='material-icons'>chevron_right</span>
+                </button>
+            </div>
+            
+            <div className='block-container'>
+                <section className='block-content'>
+                    <div className='block-title-container'>
+                        <p className='block-title'>Nuestros productos</p>
+                    </div>
+                    <ul className='homepage-categories'>
+                        {categorias.map((categoria) => (
+                            <li key={categoria.id}>
+                                <a href={categoria.ruta}>
+                                    <div>
+                                        <img src={categoria.menuImg ? categoria.menuImg[0].imgSrc : ''} alt={categoria.menuImg ? categoria.menuImg[0].imgAlt : categoria.categoria} />
+                                    </div>
+                                    <p>{categoria.categoria}</p>
+                                </a>
+                            </li>
+                        ))}
+                    </ul>
+                </section>
+            </div>
 
-            <Footer/>
+            <Footer />
         </>
     );
 }
