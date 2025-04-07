89ee13e769cda878e5916bb5fa16b1a92c129627
Leonardo Soplapuco <leonardosoplapuco@gmail.com>
2025-04-07 14:35:00 -0500

    0.4.7

--- src/Componentes/Distribuidores/Distribuidores.js
+++ src/Componentes/Distribuidores/Distribuidores.js
@@ -6,6 +6,16 @@ function Distribuidores(){
             <div className='block-content'>
                 <div className='block-title-contianer'>
                     <h2 className='block-title'>Distribuidores autorizados</h2>
+
+                    <div>
+                        <div>
+                            <ul>
+                                <li>
+                                    <img src="/assets/imagenes/componentes/distribuidores/vitai.jpg" alt="" />
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
