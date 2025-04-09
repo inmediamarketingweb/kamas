e1d0ffb230db32d4cd6236808c107229c34f72e9
Leonardo Soplapuco <leonardosoplapuco@gmail.com>
2025-04-09 10:05:30 -0500

    0.5.2

--- src/Componentes/Dormitorio/Dormitorio.js
+++ src/Componentes/Dormitorio/Dormitorio.js
@@ -3,21 +3,41 @@ import './Dormitorio.css';
 function Dormitorio(){
     return(
         <div className='block-container dormitorio-block-container'>
-            <div className='block-content dormitorio-block-content'>
-                <div className='block-title-container'>
-                    <h2 className='block-title'>Más productos</h2>
-                </div>
-
+            <div className='block-content'>
                 {/* <img src="/assets/imagenes/componentes/dormitorio/fondo.jpg" alt="" className='fondo'/> */}
 
-                <img src="/assets/imagenes/componentes/dormitorio/dormitorio.png" alt="" className='dormitorio'/>
-                <div className='dormitorio-button-container'>
-                    <button className='dormitorio-button'>+</button>
-                    <a href='/' className=''>
-                        <img src="/assets/imagenes/componentes/dormitorio/dormitorio.png" alt=""/>
-                        <span>KAMAS</span>
-                        <p>DORMITORIO KAMAS KING SARKI</p>
-                    </a>
+                <div className='dormitorio-container'>
+                    <div className='dormitorio-content'>
+                        <div className='dormitorio-cabecera-container'>
+                            <div className='dormitorio-cabecera'>
+                                <img src="/assets/imagenes/componentes/dormitorio/cabecera.png" alt="" className=''></img>
+                            </div>
+                        </div>
+
+                        <div className='dormitorio-cama-box-container'>
+                            <div className='dormitorio-cama-box'>
+                                <img src="/assets/imagenes/componentes/dormitorio/cama-box-tarima.png" alt="" className=''></img>
+                            </div>
+                        </div>
+
+                        <div className='dormitorio-colchon-container'>
+                            <div className='dormitorio-colchon'>
+                                <img src="/assets/imagenes/componentes/dormitorio/colchon-sarki.png" alt="" className=''></img>
+                            </div>
+                        </div>
+
+                        <div className='dormitorio-velador-container'>
+                            <div className='dormitorio-velador'>
+                                <img src="/assets/imagenes/componentes/dormitorio/velador.png" alt="" className=''></img>
+                            </div>
+                        </div>
+
+                        <div className='dormitorio-puff-container'>
+                            <div className='dormitorio-puff'>
+                                <img src="/assets/imagenes/componentes/dormitorio/puff-circular.png" alt="" className=''></img>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
