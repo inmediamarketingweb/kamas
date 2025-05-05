73f26ea2db44e2518d0a51998002936bb4248329
Leonardo Soplapuco <leonardosoplapuco@gmail.com>
2025-05-05 11:15:04 -0500

    0.7.9

--- src/Paginas/Error404/Error404.js
+++ src/Paginas/Error404/Error404.js
@@ -19,6 +19,7 @@ const Error404 = () => {
                 <div className="block-container">
                     <section className="block-content d-flex-center-center d-flex-column gap-20 not-found-content">
                         <h1 className="block-title">Página no encontrada</h1>
+                        <p className="text">Este sitio web sigue en estapa de desarrollo por lo que puede que algunos productos cuenten con información errorea o algunas página no existan todavía por favor consulte con nuestro soporte cualquier inconvenientes.</p>
                         <p className="text">Lo sentimos esta página está en mantenimiento o ya no existe, cualquier consulta escribenos a <a href="mailto: soporte@kamas.pe" className="text color-color-1">soporte@kamas.pe</a></p>
                     </section>
                 </div>
